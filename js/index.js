736d8e3fd9284036023811479115448a5dc0a146
mayasundar <mayasundar2@gmail.com>
2021-01-08 11:21:47 -0500

    finished gallery

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
-  $('p').on('click',function()){
-    $(this).html('hi');
+
+  $(window).scroll( function(){
+
+      /* Check the location of each desired element */
+      $('container').each( function(i){
+
+          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+          /* If the object is completely visible in the window, fade it it */
+          if( bottom_of_window > bottom_of_object ){
+
+              $(this).animate({'opacity':'1'},500);
+
+          }
+
+      });
+
   });
 
 
