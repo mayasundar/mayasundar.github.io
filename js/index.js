9d55d23383af2e35d6c38bd828411ca3391b9690
mayasundar <mayasundar2@gmail.com>
2021-01-08 15:44:53 -0500

    changed css, added js scroll

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
-$(document).ready(function(){
 
-  $(window).scroll( function(){
+  $(document).ready(function() {
 
-      /* Check the location of each desired element */
-      $('container').each( function(i){
+      $(window).scroll( function(){
 
-          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-          var bottom_of_window = $(window).scrollTop() + $(window).height();
+          $('.fadein').each( function(i){
 
-          /* If the object is completely visible in the window, fade it it */
-          if( bottom_of_window > bottom_of_object ){
+              var bottom_of_element = $(this).offset().top + $(this).outerHeight();
 
-              $(this).animate({'opacity':'1'},500);
+              var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-          }
+
+              if( bottom_of_window > bottom_of_element ){
+
+                  $(this).animate({'opacity':'1'},1000);
+
+              }
+
+          });
 
       });
 
   });
-
-
-});
