418a9a70a0fcf05dc90e4831ef409bb4902847fa
mayasundar <mayasundar2@gmail.com>
2021-07-27 20:39:52 -0400

    updated npm dependencies

--- mayasundar/src/pages/{MarkdownRemark.frontmatter__slug}.js
+++ mayasundar/src/pages/{MarkdownRemark.frontmatter__slug}.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
+import "../styles/style.css"
+
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
@@ -8,8 +10,10 @@ export default function Template({
   return (
     <div className="blog-post-container">
       <div className="blog-post">
+      <div className="divider">
         <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
+        <h3>{frontmatter.date}</h3>
+        </div>
         <div
           className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: html }}
