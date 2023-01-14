0716fae7d6bb0ce2b6c18f91662c70bc1539c7b4
mayasundar <mayasundar2@gmail.com>
2023-01-14 00:52:31 -0500

    commit 2023 site

---
+++ mayasundar.github.io/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React from "react";
+import Home from "./pages/Home";
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+
+function App() {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home/>} exact />
+
+                </Routes>
+            </BrowserRouter>,
+        </>
+    );
+}
+
+
+
+
+export default App;
