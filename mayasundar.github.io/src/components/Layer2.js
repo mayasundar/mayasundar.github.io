1bf870b485514964c248cd1162bbe3be88c69baf
mayasundar <64454649+mayasundar@users.noreply.github.com>
2024-01-11 23:58:29 -0500

    upload images

--- mayasundar.github.io/src/components/Layer2.js
+++ mayasundar.github.io/src/components/Layer2.js
@@ -1,27 +1,52 @@
 import React from "react";
 import Button from "./Button"
+import mayapaintstudio from "../assets/mayapaintstudio.jpeg"
+import studentstay from "../assets/joinstudentstay.jpeg"
 
-function Layer2() {
-    const childTexts = [
-        "mayapaint.studio",
-        "mond",
-        "Annetta Homes",
-        "StudentStay",
-        "This website"
-    ];
-
+function Layer2(){
     return (
         <div className="tab">
                 <div className="list-tab-content">
 
                 <div className="tab-text">
                 <h1> Things I've Worked On ☺</h1>
-                        {childTexts.map((text, index) => (
-                            <div className="child" key={index}>
-                                {text}
-                            </div>
-                        ))}
-            </div>
+                    <div className="box">
+                        <p>Observability App</p>
+                        <li>A simple posting web app integrated with application performance monitoring and log shipping tools like Prometheus, Filebeat, Grafana, and Elastic.</li>
+                    </div>
+
+                    <div className="box">
+                        <p>mayapaint.studio</p>
+                        <img src={mayapaintstudio} alt="mayapaintstudio" height={200}/>
+
+                    </div>
+
+                    <div className="box">
+                        <p>StudentStay</p>
+                        <img src={studentstay} alt="student stay screenshot" height={200}/>
+
+                    </div>
+
+
+                    <div className="box">
+                        <p>mond</p>
+
+                    </div>
+                    <div className="box">
+                        <p>Annetta Homes</p>
+
+                    </div>
+                    <div className="box">
+                        <p>Letterboxd Database</p>
+
+                    </div>
+                    <div className="box">
+                        <p>OutGoing</p>
+
+                    </div>
+
+
+                </div>
         </div>        </div>
 
             );
