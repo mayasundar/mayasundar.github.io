eeefa629fa6f4ff8c41a36f4d97bf35e05c3e9b1
maya :) <64454649+mayasundar@users.noreply.github.com>
2025-03-17 16:59:15 -0400

    break up projects page (to add new views later)

---
+++ src/app/projects/list.tsx
@@ -0,0 +1,77 @@
+"use client";
+import { useSidebar } from "../../components/SidebarContext";
+import { projectsData } from "./projectData";
+import Image from "next/image";
+
+const List = () => {
+    const { visibleComponents, setActiveGroup } = useSidebar();
+    const handleTagClick = (tag) => {
+        const validTags = ["all", "web", "mobile", "design", "brand"];
+        if (validTags.includes(tag)) {
+            setActiveGroup(tag);
+        }
+    };
+
+    return (
+        <div>
+            <h3>Projects ☺&#xFE0E;</h3>
+            <div>
+                {Object.entries(projectsData).map(
+                    ([key, project]) =>
+                        visibleComponents[key] && (
+                            <div key={key} data-component-id={key}>
+                                <div className="tbox ">
+
+                                    <div className="flex items-baseline gap-6">
+                                        <div>
+                                            <h3>{project.name}</h3>
+                                        </div>
+                                        <div >
+                                            {project.websiteUrl && (
+                                                <a
+                                                    className="website"
+                                                    href={project.websiteUrl}
+                                                    target="_blank"
+                                                    rel="noopener noreferrer"
+                                                >
+                                                    {project.websiteText || "website"}
+                                                </a>
+                                            )}
+                                        </div>
+                                    </div>
+
+                                    <div className="flex items-baseline map">
+                                        {project.tags.map((tag, index)=>(
+                                            <a key={index}
+                                               className="tag"
+                                               onClick={() => handleTagClick(tag)}
+                                            >{tag}</a>
+                                        ))}
+                                    {project.tools.map((tool, index) => (
+                                        <div key={index} className="tool"
+                                        >{tool}</div>
+                                    ))}
+                                    </div>
+
+                                    <div>
+                                        {project.description.map((desc, index) => (
+                                            <p key={index}>{desc}</p>
+                                        ))}
+                                    </div>
+
+                                    <div>
+                                        <Image
+                                            src={project.image}
+                                            alt={project.imageAlt}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default List;
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,75 +1,12 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSidebar, ProjectTag } from "../../components/SidebarContext";
-import HibiscusSans from "./layers/HibiscusSans";
-import ColumbiaInTech from "./layers/ColumbiaInTech";
-import StudentStay from "./layers/StudentStay";
-import ObservabilityApp from "./layers/ObservabilityApp";
-import OutGoing from "./layers/OutGoing";
-import AnnettaHomes from "./layers/AnnettaHomes";
-import FourQuadrants from "./layers/FourQuadrants";
-import MayaSundar from "./layers/MayaSundar";
-import MayaPaintStudio from "./layers/MayaPaintStudio";
-import Mond from "./layers/Mond";
-
-const projectsData = {
-    "HibiscusSans": {
-        name: "Hibiscus Sans",
-        tags: ["all", "design", "web"] as ProjectTag[]
-    },
-    "ColumbiaInTech": {
-        name: "Columbia in Tech",
-        tags: ["all", "web", "brand"] as ProjectTag[]
-    },
-    "StudentStay": {
-        name: "Student Stay",
-        tags: ["all", "web", "brand", "design"] as ProjectTag[]
-    },
-    "ObservabilityApp": {
-        name: "Observability App",
-        tags: ["all", "web"] as ProjectTag[]
-    },
-    "OutGoing": {
-        name: "OutGoing",
-        tags: ["all", "mobile", "design"] as ProjectTag[]
-    },
-    "MayaPaintStudio": {
-        name: "mayapaint.studio",
-        tags: ["all", "mobile"] as ProjectTag[],
-    },
-    "AnnettaHomes": {
-        name: "Annetta Homes",
-        tags: ["all", "web", "design", "brand"] as ProjectTag[],
-    },
-    "Mond": {
-        name: "mond",
-        tags: ["all", "mobile"] as ProjectTag[],
-    },
-    "MayaSundar": {
-        name: "mayasundar.com",
-        tags: ["all", "web"] as ProjectTag[],
-    },
-    "FourQuadrants": {
-        name: "Four Quadrants",
-        tags: ["all", "web"] as ProjectTag[],
-    },
-
-};
-
-const componentsList: Record<string, React.ComponentType> = {
-    HibiscusSans,
-    FourQuadrants,
-    ColumbiaInTech,
-    StudentStay,
-    ObservabilityApp,
-    OutGoing,
-    MayaPaintStudio,
-    AnnettaHomes,
-    Mond,
-    MayaSundar,
-};
+import List from "@/app/projects/list";
+import Grid from "@/app/projects/grid";
+import {componentsList, projectsData} from "./projectData";
 
 const Projects = () => {
+    const [activeView, setActiveView] = useState<ViewType>("list");
     const { visibleComponents, initializeComponents } = useSidebar();
     const initialized = useRef(false);
 
@@ -77,7 +14,7 @@ const Projects = () => {
         if (!initialized.current) {
             initialized.current = true;
             initializeComponents(
-                Object.keys(componentsList),
+                Object.keys(projectsData),
                 "projects",
                 projectsData
             );
@@ -86,17 +23,31 @@ const Projects = () => {
 
     return (
         <div>
-            <h3>Projects ☺&#xFE0E;</h3>
-
-            <div className="tbox">
-                {Object.entries(componentsList).map(
-                    ([key, Component]) =>
-                        visibleComponents[key] && (
-                            <div key={key} data-component-id={key}>
-                                <Component />
-                            </div>
-                        )
-                )}
+            {/*<h3>Projects ☺&#xFE0E;</h3>*/}
+            <div className="flex space-x-2">
+                {/*<button*/}
+                {/*    onClick={() => setActiveView("list")}*/}
+                {/*    className={`px-4 py-2 rounded ${*/}
+                {/*        activeView === "list"*/}
+                {/*            ? "bg-blue-500 text-white"*/}
+                {/*            : "bg-gray-200"*/}
+                {/*    }`}*/}
+                {/*>*/}
+                {/*    List View*/}
+                {/*</button>*/}
+                {/*<button*/}
+                {/*    onClick={() => setActiveView("grid")}*/}
+                {/*    className={`px-4 py-2 rounded ${*/}
+                {/*        activeView === "grid"*/}
+                {/*            ? "bg-blue-500 text-white"*/}
+                {/*            : "bg-gray-200"*/}
+                {/*    }`}*/}
+                {/*>*/}
+                {/*    Grid View*/}
+                {/*</button>*/}
+            </div>
+            <div className="flex-grow">
+                {activeView === "list" ? <List /> : <Grid />}
             </div>
             </div>
     );
