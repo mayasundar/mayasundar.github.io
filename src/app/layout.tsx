2ca67e142bdb97614c8a810ca433470d7deb7e15
maya :) <64454649+mayasundar@users.noreply.github.com>
2025-03-17 16:57:46 -0400

    color, buttons, svg changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-import type { Metadata } from "next";
+"use client"
 import "./globals.css";
 import localFont from "next/font/local";
-import Sidebar from "../components/Sidebar";
 import React from "react";
+import SidebarWrapper from "@/components/SidebarWrapper";
+import {useSearchParams} from "next/navigation";
 
 const hibiscus = localFont({
     src: "../../public/HibiscusSans-Regular.otf",
@@ -10,19 +11,28 @@ const hibiscus = localFont({
     display: 'swap',
 });
 
-export const metadata: Metadata = {
-    title: "Maya Sundar",
-    description: "My personal website and project portfolio.",
-};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+    const searchParams = useSearchParams();
+    const isMini = searchParams.get("mini") === "true";
+
     return (
         <html lang="en">
+        {/*<body*/}
+        {/*    className={`${hibiscus.className}  bg-[#F9F6F6] text-[#5D5D5D] leading-[1.4] bg-[length:40px_40px] bg-[radial-gradient(circle,rgba(101,116,141,0.46)_1px,rgba(0,0,0,0)_1px)]`}*/}
+        {/*>*/}
         <body
-            className={`${hibiscus.className}  bg-[#EEEEEE] text-[#65748D] leading-[1.4] bg-[length:40px_40px] bg-[radial-gradient(circle,rgba(101,116,141,0.46)_1px,rgba(0,0,0,0)_1px)]`}
+            className={`${hibiscus.className}  bg-[#F9F6F6] text-[#5D5D5D] leading-[1.4] bg-[length:40px_40px] bg-[radial-gradient(circle,rgba(93,93,93,0.46)_1px,rgba(0,0,0,0)_1px)]`}
         >
-            {/*<Navbar />*/}
-            <Sidebar>{children}</Sidebar>
+
+        {/*<body*/}
+        {/*    className={`${hibiscus.className}  bg-[#F9F6F6] text-[#5D5D5D] leading-[1.4] bg-[length:20px_20px] bg-[linear-gradient(to_right,rgba(231,229,220,0.26)_1px,transparent_1px),linear-gradient(to_bottom,rgba(231,229,220,0.26)_1px,transparent_1px)]`}*/}
+        {/*>*/}
+        {/*<body*/}
+        {/*    className={`${hibiscus.className}  bg-[#F9F6F6] text-[#5D5D5D] leading-[1.4] bg-[length:30px_30px] bg-[radial-gradient(circle,rgba(212,211,203)_1px,rgba(0,0,0,0)_1px)]`}*/}
+        {/*>*/}
+
+        {isMini ? children : <SidebarWrapper>{children}</SidebarWrapper>}
         </body>
         </html>
     );
