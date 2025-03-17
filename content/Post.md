7eb6a1293f9ebbffdfbc78b88bab9b9837fdba37
maya :) <64454649+mayasundar@users.noreply.github.com>
2025-03-17 16:27:26 -0400

    basic markdown processing + test post

---
+++ content/Post.md
@@ -0,0 +1,8 @@
+---
+title: 'Testing'
+date: '2025-03-03'
+excerpt: 'This is my first blog post'
+---
+
+# Test [replace]
+
---
+++ lib/markdown.ts
@@ -0,0 +1,70 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import { remark } from 'remark';
+import html from 'remark-html';
+
+const postsDirectory = path.join(process.cwd(), '/content');
+
+export function getAllPostSlugs() {
+    const fileNames = fs.readdirSync(postsDirectory);
+
+    return fileNames.map(fileName => {
+        return {
+            params: {
+                slug: fileName.replace(/\.md$/, '')
+            }
+        };
+    });
+}
+
+export function getPostData(slug) {
+    const fullPath = path.join(postsDirectory, `${slug}.md`);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+    const matterResult = matter(fileContents);
+
+    return {
+        slug,
+        ...matterResult.data,
+        content: matterResult.content
+    };
+}
+
+export async function getPostWithHTML(slug) {
+    const post = getPostData(slug);
+    const processedContent = await remark()
+        .use(html)
+        .process(post.content);
+
+    const contentHtml = processedContent.toString();
+    return {
+        ...post,
+        contentHtml
+    };
+}
+
+export function getAllPosts() {
+    const fileNames = fs.readdirSync(postsDirectory);
+    const allPostsData = fileNames.map(fileName => {
+        const slug = fileName.replace(/\.md$/, '');
+
+        const fullPath = path.join(postsDirectory, fileName);
+        const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+        const matterResult = matter(fileContents);
+
+        return {
+            slug,
+            ...matterResult.data
+        };
+    });
+
+    return allPostsData.sort((a, b) => {
+        if (a.date < b.date) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+}
