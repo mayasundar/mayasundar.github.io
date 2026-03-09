3a9e4439b1139910f25afdab0c007a627253a2ad
maya :) <64454649+mayasundar@users.noreply.github.com>
2026-03-09 14:37:26 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,6 @@ import type { Config } from 'tailwindcss'
 
 export default {
     content: [
-        './app/**/*.{js,ts,jsx,tsx,mdx}',
-        './pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './layers/**/*.{js,ts,jsx,tsx,mdx}',
-
-        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-
-
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
         "./sanity/**/*.{js,ts,jsx,tsx}",
